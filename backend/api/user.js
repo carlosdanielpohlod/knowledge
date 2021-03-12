936ca20d730de5e72469b0eb55e01ff9d32f3c91
carlosdanielpohlod <carlospohlod@gmail.com>
2021-03-12 10:32:51 -0300

    the insert and update user has been created

--- backend/api/user.js
+++ backend/api/user.js
@@ -9,6 +9,33 @@ module.exports = app => {
     }
     const save = async (req, res) =>{
         const user = {...req.body}
+        if(req.params.id) user.id = req.params.id
+        try{
+            const userDB = await app.db('users').where({email:user.email}).first()
+            if(!user.id){
+                notExistsOrError(userDB, 'Usuario ja cadastrado')
+            }
+        
+        }
+        catch(msg){
+            res.status(400).send(msg)
+        }
+        user.password = encryptPassword(user.password)
+        delete user.confirmPassword
+
+        if(user.id){
+            app.db('users')
+                .update(user)
+                .where({id: user.id})
+                .then(_ => res.status(400).send())
+                .catch(err => res.status(500).send(err))
+        }
+        else{ 
+            app.db('users')
+                .insert(user)
+                .then(_ => res.status(201).send())
+                .catch(err => res.status(500).send(err))
+        }
     }
 
     return {save}
