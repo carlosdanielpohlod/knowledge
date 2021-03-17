176c8eeaba2d41874ce320f86b6af378d1513113
carlosdanielpohlod <carlospohlod@gmail.com>
2021-03-17 15:56:26 -0300

    Version with a consign bug

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,13 @@ const db = require('./config/db')
 
 app.db = db
 consign()
+    
+    .include('./api/validation.js')
+    .then('./api/user.js')
     .then('./config/middlewares.js')
-    .then('./api')
     .then('./config/routes.js')
     .into(app)
+
 app.listen(3000, () => {
     console.log("Backend executando")
 })
